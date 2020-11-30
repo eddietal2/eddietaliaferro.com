626e6c065076a9ee67b4632fe535d3a508a574ce
Eddie Taliaferro <eddielacrosse2@gmail.com>
2020-11-29 23:18:56 -0500

    Logos, Install Prompt Updates

--- eddietaliaferro.com/src/app/pages/home/home.page.ts
+++ eddietaliaferro.com/src/app/pages/home/home.page.ts
@@ -25,18 +25,34 @@ export class HomePage implements OnInit {
     const deviceType = window.navigator.userAgent;
     console.log(deviceType);
 
-    // Apple
     // Safari Desktop
-    if (deviceType.search('Safari') > -1) {
+    if (deviceType.search('Safari' && 'Version') > -1) {
       console.log('Safari');
       this.downloadButton.innerHTML = 'Safari';
     }
-    // iPhone Safari
-    // The word Version is the only difference between the iOS Chrome and Safari User Agents
-    //Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1
+     // Chrome Desktop
+    if (deviceType.search('Chrome') > -1) {
+      this.downloadButton.innerHTML = 'Chrome';
+    }
+    // Firefox Desktop
+    if (deviceType.search('Firefox') > -1) {
+      this.downloadButton.innerHTML = 'Firefox';
+    }
+    // Edge Desktop
+    if (deviceType.search('Edg') > -1) {
+      this.downloadButton.innerHTML = 'Edge';
+    }
+    // Opera Desktop
+    if (deviceType.search('OPR') > -1) {
+      console.log('Opera');
+      this.downloadButton.innerHTML = 'Opera';
+    }
 
-    if (deviceType.search('Version' && 'iPhone') > -1) {
+    // iOS
+    // (Mobile) Safari iOS
+    if (deviceType.search('Safari' && 'Version' && 'Mobile') > -1) {
       this.downloadButton.innerHTML = 'iOS Safari';
+      this.downloadButton.style.display = 'block';
       this.downloadButton.addEventListener('click', () => {
         console.log('Clicked Button');
         this.iPhoneInstallBanner.style.display = 'block';
@@ -61,6 +77,7 @@ export class HomePage implements OnInit {
     // (Mobile) FireFox iOS
     if (deviceType.search('FxiOS') > -1) {
       this.downloadButton.innerHTML = 'iOS Firefox';
+      this.downloadButton.style.display = 'block';
       this.downloadButton.addEventListener('click', () => {
         console.log('Clicked Button');
         this.openSafariBanner.style.display = 'block';
@@ -74,6 +91,7 @@ export class HomePage implements OnInit {
     // (Mobile) Edge iOS
     if (deviceType.search('EdgiOS') > -1) {
       this.downloadButton.innerHTML = 'iOS Edge';
+      this.downloadButton.style.display = 'block';
       this.downloadButton.addEventListener('click', () => {
         this.openSafariBanner.style.display = 'block';
         this.iPhoneInstallBanner.style.display = 'none';
@@ -86,6 +104,7 @@ export class HomePage implements OnInit {
     // (Mobile) Opera iOS
     if (deviceType.search('OPT') > -1) {
       this.downloadButton.innerHTML = 'iOS Opera';
+      this.downloadButton.style.display = 'block';
       this.downloadButton.addEventListener('click', () => {
         console.log('Clicked Button');
         this.openSafariBanner.style.display = 'block';
@@ -97,77 +116,89 @@ export class HomePage implements OnInit {
       });
     }
 
-
-
     // iPad
     if (deviceType.search('iPad') > -1) {
       this.downloadButton.innerHTML = 'iPad';
+      this.downloadButton.style.display = 'block';
     }
 
     // Android
-    if (deviceType.search('Chrome') > -1) {
-      console.log('Chrome Browser');
-      this.downloadButton.innerHTML = 'Chrome';
-
+    // (Mobile) Chrome Android
+    if (
+      deviceType.search('Chrome') > -1 &&
+      deviceType.search('Android') > -1
+      ) {
+      this.downloadButton.innerHTML = 'Chrome Android';
+      this.downloadButton.addEventListener('click', () => {
+        this.iPhoneInstallBanner.style.display = 'none';
+        this.showInstallBanner();
+      });
     }
-    // if (deviceType.search('Chrome' && 'Mobile' && 'Android') > -1) {
-    //   console.log('Chrome Browser');
-    //   this.downloadButton.innerHTML = 'Android';
-    // }
+    // (Mobile) Firefox Android
+    if (
+      deviceType.search('Firefox') > -1 &&
+      deviceType.search('Android') > -1) {
 
-    // Firefox
-    if (deviceType.search('Firefox') > -1) {
-      console.log('Firefox Browser');
-      this.downloadButton.innerHTML = 'Firefox';
+        this.downloadButton.innerHTML = 'Firefox Android';
     }
+    // (Mobile) Edge Android
+    if (
+      deviceType.search('EdgA') > -1) {
+      this.downloadButton.innerHTML = 'Edge Android';
+    }
+    // (Mobile) Opera Mini Android
 
-    // Edge
-    // if (deviceType.search('Edg') > -1) {
-    //   this.downloadButton.innerHTML = 'Edge';
-    // }
     // Samsung Browser
-    // if (deviceType.search('Firefox') > -1) {
+    if (deviceType.search('SamsungBrowser') > -1) {
+      this.downloadButton.innerHTML = 'Samsung Browser';
+    }
 
-    // }
+    // Check to see if the app is already installed on the users device
+    console.log(window.navigator);
+    
 
-    // Opera
-    // if (deviceType.search('OPR') > -1) {
-    //   console.log('Opera');
-    //   this.downloadButton.innerHTML = 'Opera';
-    // }
-
-
+    // Chrome, Edge,
     window.addEventListener('beforeinstallprompt', (e) => {
       console.log('beforeinstallprompt Event fired');
       // Prevent Chrome 67 and earlier from automatically showing the prompt
       e.preventDefault();
       // Stash the event so it can be triggered later.
       this.deferredPrompt = e;
+      if (this.deferredPrompt) {
+        this.downloadButton.style.display = 'block';
+        this.downloadButton.addEventListener('click', () => {
+          this.showInstallBanner();
+        });
+      }
       console.log('This is the stashed event');
       console.log(e);
 });
 
-    // Determine if the app is install
-    window.addEventListener('appinstalled', (event) => {
-      console.log('installed');
-      console.log(event);
-    });
-
     // Detect if app is launched from home screen
     if (window.matchMedia('(display-mode: standalone)').matches) {
       console.log('display-mode is standalone');
+      this.iPhoneInstallBanner.style.display = 'none';
+      this.openSafariBanner.style.display = 'none';
+      this.downloadButton.style.display = 'none';
     }
     // Detect if app is launched from home screen
     // Safari
-    // if (window.navigator.standalone === true) {
-    //   console.log('display-mode is standalone');
-    // }
+    if (window.navigator['standalone'] === true) {
+      console.log('display-mode is standalone');
+      this.iPhoneInstallBanner.style.display = 'none';
+      this.openSafariBanner.style.display = 'none';
+      this.downloadButton.style.display = 'none';
+    }
   }
 
   showInstallBanner() {
 
     console.log('Trying to Show Install Banner ...');
     console.log(this.deferredPrompt);
+    if (this.deferredPrompt === undefined) {
+      console.log('This page was already installed');
+      this.downloadButton.style.display = 'none';
+    }
     if (this.deferredPrompt !== undefined && this.deferredPrompt !== null) {
       // Hide Download Button
       this.downloadButton.style.display = 'none';
@@ -180,6 +211,7 @@ export class HomePage implements OnInit {
           console.log('User accepted the A2HS prompt');
         } else {
           this.downloadButton.style.display = 'block';
+          this.downloadButton.style.margin = '0 auto 90px auto';
           console.log('User dismissed the A2HS prompt');
         }
         // We no longer need the prompt.  Clear it up.
