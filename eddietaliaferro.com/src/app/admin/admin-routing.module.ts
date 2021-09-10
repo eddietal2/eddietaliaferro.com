58c8c1db1ffb5a14df24f43d064d0e8376235694
Eddie Taliaferro <eddielacrosse2@gmail.com>
2021-09-10 18:28:55 -0400

    added all admin pages with routing

--- eddietaliaferro.com/src/app/admin/admin-routing.module.ts
+++ eddietaliaferro.com/src/app/admin/admin-routing.module.ts
@@ -6,8 +6,27 @@ import { AdminPage } from './admin.page';
 const routes: Routes = [
   {
     path: '',
-    component: AdminPage
-  }
+    component: AdminPage,
+    children: [
+      {
+        path: 'blogs',
+        pathMatch: '',
+        loadChildren: () => import('./blogs/blogs.module').then( m => m.BlogsPageModule)
+      },
+      {
+        path: 'projects',
+        loadChildren: () => import('./projects/projects.module').then( m => m.ProjectsPageModule)
+      },
+      {
+        path: 'contact',
+        loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
+      },
+      {
+        path: 'donate',
+        loadChildren: () => import('./donate/donate.module').then( m => m.DonatePageModule)
+      }
+    ]
+  },
 ];
 
 @NgModule({
