6cddc236506c62f4482b9ed1ef900fbb6fedd797
Eddie Taliaferro <eddielacrosse2@gmail.com>
2021-09-16 00:39:39 -0400

    added Donate page, and added initial FormBuilder logic

--- eddietaliaferro.com/src/app/pages/contact/contact.page.ts
+++ eddietaliaferro.com/src/app/pages/contact/contact.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastController, LoadingController, IonInput, IonSpinner, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.page.scss'],
 })
 export class ContactPage implements OnInit {
+  contactForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.contactForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      message: ['', [Validators.required,]]
+    });
+  }
+  onSubmit() {
+    
+  }
+  donatePage() {
+
   }
 
 }
--- eddietaliaferro.com/src/app/pages/donate/donate.page.ts
+++ eddietaliaferro.com/src/app/pages/donate/donate.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastController, LoadingController, IonInput, IonSpinner, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-donate',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./donate.page.scss'],
 })
 export class DonatePage implements OnInit {
+  donateForm: FormGroup
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.donateForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      dollar_amount: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      message: ['', [Validators.required,]]
+    });
+  }
+  onSubmit() {
+
+  }
+  contactPage() {
+
   }
 
 }
