7fdb0d3c67130d3bbbe28baa2e113077b3952775
Eddie Taliaferro <eddielacrosse2@gmail.com>
2021-10-02 01:07:35 -0400

    added baseline illustration svgs, small ui updates, fixed initial active route button

--- eddietaliaferro.com/src/app/app-routing.module.ts
+++ eddietaliaferro.com/src/app/app-routing.module.ts
@@ -4,10 +4,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
