9e3e0bfae97b0be317ee361b1032cb1b80cf6bdc
Eddie Taliaferro <eddielacrosse2@gmail.com>
2021-10-24 18:13:03 -0400

    Refined Blogs illustration, refined Gold E Logo

--- eddietaliaferro.com/src/app/components/blog-illustration/blog-illustration.component.ts
+++ eddietaliaferro.com/src/app/components/blog-illustration/blog-illustration.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./blog-illustration.component.scss'],
 })
 export class BlogIllustrationComponent implements OnInit {
+  // turn #path-1 to #path-1000
+  // turn linearGradient 4 to 456
+  // turn linearGradient 1 to 123
   page;
   pencil;
   blogLineOne;
@@ -24,17 +27,27 @@ export class BlogIllustrationComponent implements OnInit {
       // Get Pencil SVG Elements
       console.clear();
       this.pencil = document.getElementById('Pencil');
+      this.page = document.getElementById('Page');
+
       this.blogLineOne = document.getElementById('blog-line-1');
       this.blogLineTwo = document.getElementById('blog-line-2');
       this.blogLineThree = document.getElementById('blog-line-3');
-      this.page = document.getElementById('Page');
 
       this.blogLineOneLength = this.blogLineOne.getTotalLength();
       this.blogLineTwoLength = this.blogLineTwo.getTotalLength();
       this.blogLineThreeLength = this.blogLineThree.getTotalLength();
-    }
+
+      console.log(this.blogLineOne);
+      console.log(this.blogLineOneLength);
+      console.log('\n');
+      console.log(this.blogLineTwo);
+      console.log(this.blogLineTwoLength);
+      console.log('\n');
+      console.log(this.blogLineThree);
+      console.log(this.blogLineThreeLength);
 
 
   }
+}
 
 }
