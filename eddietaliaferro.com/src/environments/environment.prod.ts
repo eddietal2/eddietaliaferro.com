4b996b9a2d7983ba23051c7b14984e19e56fe83b
Eddie Taliaferro <eddielacrosse2@gmail.com>
2021-10-27 04:37:18 -0400

    -

--- eddietaliaferro.com/src/environments/environment.prod.ts
+++ eddietaliaferro.com/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+const ip = '169.62.226.194';
+const baseUrl = `http://${ip}:3000`;
+const stripeKeyLive = 'pk_live_51JfTbQEo9mAig2o8WBjwlzGV5JPOjLICIj7zGlVqlKFbloDjnXJxw7fauYLTGfYPyGlVDdaN7ThdUg75mwVbXJRY00fP3MSXx6';
+const stripeKeyTest = 'pk_test_51JfTbQEo9mAig2o8V8YUYYdgLtvnvzWvVfnUnnySoAmrCP7AdEvnJ6AGBGbQf6lYGUiKOCMurhKUFf9p6odalfmi00ojrGPFVe';
+
 export const environment = {
-  production: true
+  production: true,
+  url: baseUrl,
+  stripeKey: stripeKeyTest
 };
